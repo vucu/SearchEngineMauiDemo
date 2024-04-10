e430494c47170ee8a6b2d6fb55ac0906781798dd
Vu Cu <cxv@g.ucla.edu>
2024-04-10 08:35:53 -0700

    initial commit

---
+++ LocalSearchEngine/LocalSearchEngine/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalSearchEngine
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LocalSearchEngine/LocalSearchEngine/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LocalSearchEngine
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
